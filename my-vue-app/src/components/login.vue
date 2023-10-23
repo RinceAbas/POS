f8a3b937474b3aa269c6c833d48d3edcb19a6439
BSIT2A-abas <rabas_220000001580@uic.edu.ph>
2023-10-23 09:27:10 +0800

    first commit

---
+++ my-vue-app/src/components/login.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h1>Login</h1>
+        <form @submit.prevent="login">
+            <div>
+                <label for="email">Email:</label>
+                <input type="email" id="email" v-model="email" required>
+            </div>
+            <div>
+                <label for="password">Password:</label>
+                <input type="password" id="password" v-model="password" required>
+            </div>
+            <button type="submit">Login</button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            email: '',
+            password: ''
+        }
+    },
+    methods: {
+        login() {
+            // perform login logic here
+            console.log(`Logged in with email: ${this.email} and password: ${this.password}`);
+        }
+    }
+}
+</script>
